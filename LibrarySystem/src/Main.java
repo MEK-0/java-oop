a5e5aa2c2a04f80e266d618ae7ea88a0b65e73b9
Mahmut Esat <74824886+MEK-0@users.noreply.github.com>
2024-12-06 15:21:18 +0300

    Add files via upload

---
+++ LibrarySystem/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Library kutup= new Library();
+        kutup.x=10;
+
+        System.out.println(kutup.x);
+
+    }
+}
